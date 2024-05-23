b8cd3cc0f8ae8aa2224dc847646afe6ca4f8dcf9
zmrocze <karolochmanmilarski@gmail.com>
2024-05-23 19:30:47 +0200

    Error: shell doesn't build 'containers conflicts with vector'

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,12 @@
 module MyLib (someFunc) where
 
+import Data.Map.Strict (Map)
+
+-- datatype Date = Date { year :: Int, month :: Int, day :: Int }
+
+newtype HabitsData = HabitsData {directories :: Map String Checkmarks}
+
+newtype Checkmarks = Checkmarks {checkmarks :: Vector (Date, Bool)}
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
